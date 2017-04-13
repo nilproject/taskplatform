4339263d70b303e98587484faf83ee27ec08a17c
Дмитрий <@DESKTOP-37CTHHC>
2017-04-14 01:37:38 +0700

    Набросок страницы логина

--- www/ui/loginpage/loginpage.js
+++ www/ui/loginpage/loginpage.js
@@ -4,5 +4,16 @@
     "./loginpage.css",
     [],
     function (app, element, childs, tagedNodes, params) {
+
+        var imageIndex = 0;
+        var images = tagedNodes.imageSlot[0].getElementsByTagName("img");
+        setInterval(function () {
+            $(images[imageIndex]).removeClass("current");
+
+            imageIndex++;
+            imageIndex %= images.length;
+
+            $(images[imageIndex]).addClass("current");
+        }, 8000);
     }
 );
