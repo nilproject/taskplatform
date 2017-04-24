572f69382f3e804a863bbec54611507ae21ad04d
Дмитрий <polhovich.dm@mail.ru>
2017-04-24 23:51:04 +0700

    Добавлена установка прав в sql update и скрипт создания схемы

--- sql scripts/schema.sql
+++ sql scripts/schema.sql
@@ -1,2 +1,3 @@
 CREATE SCHEMA `taskplatform` ;
-
+CREATE USER 'taskPlatform'@'localhost';
+GRANT CREATE TEMPORARY TABLES, DROP, DELETE, UPDATE, INSERT, SELECT ON taskplatform.* TO 'taskPlatform'@'localhost';
